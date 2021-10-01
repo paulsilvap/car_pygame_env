2e73ea7e5dc1f2e5278e7cf2c19c487c87cc72aa
paulsilvap <paul.silva@yachaytech.edu.ec>
2021-10-01 19:34:16 +0900

    Extend screen

--- pygame_env.py
+++ pygame_env.py
@@ -3,16 +3,19 @@ from sys import exit
 import random
 
 WINDOW_HEIGHT = int(400 * 1)
-WINDOW_WIDTH = int(400 * 1)
+WINDOW_WIDTH = int(800 * 1)
+GRID_HEIGHT = WINDOW_HEIGHT
+GRID_WIDTH = int(WINDOW_WIDTH / 2)
 BLOCK_SIZE = int(50 * 1)
 COLORS = [(255,255,204),(255,204,153),(255,102,102)]
+GRID_DIMENSION = int((GRID_HEIGHT - BLOCK_SIZE*2) / BLOCK_SIZE)
 
 grids = []
 steps = 0
 
 def drawGrid(screen, blockSize):
-    for x in range(blockSize, WINDOW_WIDTH-blockSize, blockSize):
-        for y in range(blockSize, WINDOW_HEIGHT-blockSize, blockSize):
+    for x in range(blockSize, GRID_WIDTH-blockSize, blockSize):
+        for y in range(blockSize, GRID_HEIGHT-blockSize, blockSize):
             rect = pygame.Rect(x, y, blockSize, blockSize)
             index = random.randint(0,2)
             grids.append([rect, index])
@@ -20,14 +23,21 @@ def drawGrid(screen, blockSize):
 
 pygame.init()
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+canvas = pygame.Surface((WINDOW_WIDTH,WINDOW_HEIGHT))
+subrect1 = pygame.Rect(0,0,GRID_WIDTH,GRID_HEIGHT)
+subrect2 = pygame.Rect(GRID_WIDTH,0,GRID_WIDTH,GRID_HEIGHT)
+subsurface1 = canvas.subsurface(subrect1)
+subsurface2 = canvas.subsurface(subrect2)
 
 # Car icon taken from: https://uxwing.com/car-icon/
 car_surface = pygame.image.load('car.png').convert_alpha()
 car_surface = pygame.transform.scale(car_surface,(int(BLOCK_SIZE*0.75), int(BLOCK_SIZE*0.75)))
-car_rect = car_surface.get_rect(center = (BLOCK_SIZE + (BLOCK_SIZE/2), WINDOW_WIDTH - (BLOCK_SIZE + (BLOCK_SIZE/2))))
+car_rect = car_surface.get_rect(center = (GRID_WIDTH + BLOCK_SIZE + (BLOCK_SIZE/2), GRID_HEIGHT - (BLOCK_SIZE + (BLOCK_SIZE/2))))
 
-screen.fill('Gray')
-drawGrid(screen, BLOCK_SIZE)
+subsurface1.fill('Gray')
+subsurface2.fill('Gray')
+drawGrid(subsurface2, BLOCK_SIZE)
+# pygame.draw.rect(subsurface2, 'white', car_rect)
 
 while True:
     for event in pygame.event.get():
@@ -41,18 +51,21 @@ while True:
                 for i in range(len(grids)):
                     grids[i][1] = random.randint(0,2)
             for (rect, index) in grids:
-                pygame.draw.rect(screen, COLORS[index], rect)
+                pygame.draw.rect(subsurface2, COLORS[index], rect)
             if event.key == pygame.K_UP and car_rect.top > BLOCK_SIZE * 2:
                 car_rect.top -= BLOCK_SIZE
-            elif event.key == pygame.K_DOWN and car_rect.bottom < WINDOW_WIDTH - BLOCK_SIZE * 2:
+            elif event.key == pygame.K_DOWN and car_rect.bottom < GRID_WIDTH - BLOCK_SIZE * 2:
                 car_rect.bottom += BLOCK_SIZE
-            elif event.key == pygame.K_LEFT and car_rect.left > BLOCK_SIZE * 2:
+            elif event.key == pygame.K_LEFT and car_rect.left > GRID_WIDTH + BLOCK_SIZE * 2:
                 car_rect.left -= BLOCK_SIZE
-            elif event.key == pygame.K_RIGHT and car_rect.right < WINDOW_WIDTH - BLOCK_SIZE * 2:
+            elif event.key == pygame.K_RIGHT and car_rect.right < (GRID_WIDTH * 2) - BLOCK_SIZE * 2:
                 car_rect.right += BLOCK_SIZE
 
     for (rect, _) in grids:
         pygame.draw.rect(screen, 'Black', rect, 1)
+
+    screen.blit(subsurface1, (0,0))
+    screen.blit(subsurface2, (400,0))
 
     screen.blit(car_surface,car_rect)
 
